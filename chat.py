506b1a0c37604af0008b77ae8e3017952bbabbb4
Derek Weitzel <djw8605@gmail.com>
2020-10-09 10:07:02 -0500

    Initial additions

--- chat.py
+++ chat.py
@@ -11,11 +11,14 @@ import os
 import logging
 import redis
 import gevent
-from flask import Flask, render_template
+from flask import Flask, render_template, request   
 from flask_sockets import Sockets
+import json
+import uuid
 
 REDIS_URL = os.environ['REDIS_URL']
 REDIS_CHAN = 'chat'
+XROOTD_CLIENT = 'xrootd-clients'
 
 app = Flask(__name__)
 app.debug = 'DEBUG' in os.environ
@@ -24,12 +27,11 @@ sockets = Sockets(app)
 redis = redis.from_url(REDIS_URL)
 
 
-
 class ChatBackend(object):
     """Interface for registering and updating WebSocket clients."""
 
     def __init__(self):
-        self.clients = list()
+        self.clients = {}
         self.pubsub = redis.pubsub()
         self.pubsub.subscribe(REDIS_CHAN)
 
@@ -40,9 +42,18 @@ class ChatBackend(object):
                 app.logger.info(u'Sending message: {}'.format(data))
                 yield data
 
-    def register(self, client):
+    def add_worker(self, client, client_id):
         """Register a WebSocket connection for Redis updates."""
-        self.clients.append(client)
+        # Check if we have already registered this client
+        if not redis.sismember(XROOTD_CLIENT, client_id):
+            return False
+        self.clients[client_id] = client
+        return True
+    
+    def register_worker(self, client_id, client_details):
+        redis.sadd(XROOTD_CLIENT, client_id)
+        redis.set(client_id, json.dumps(client_details))
+
 
     def send(self, client, data):
         """Send given data to the registered client.
@@ -50,7 +61,34 @@ class ChatBackend(object):
         try:
             client.send(data)
         except Exception:
-            self.clients.remove(client)
+            self.remove(client)
+        
+    def remove(self, client_id):
+        """
+        Remove the id
+        """
+        redis.srem(XROOTD_CLIENT, client_id)
+        redis.delete(client_id)
+        del self.clients[client_id]
+    
+    def get_workers(self):
+        """
+        Return all workers and details about them
+        """
+        # Get all client ids
+        client_ids = redis.smembers(XROOTD_CLIENT)
+        clients = {}
+        for client_id in client_ids:
+            client = redis.get(client_id)
+            if client is not None:
+                clients[client_id] = client
+        return clients
+
+    def get_num_workers(self):
+        """
+        Return a single integer on the number of connected workers
+        """
+        return redis.scard(XROOTD_CLIENT)
 
     def run(self):
         """Listens for new messages in Redis, and sends them to clients."""
@@ -70,6 +108,14 @@ chats.start()
 def hello():
     return render_template('index.html')
 
+@app.route('/getclients')
+def get_clients():
+    return chats.get_workers()
+
+@app.route('/getnumclients')
+def get_num_clients():
+    return chats.get_num_workers()
+
 @sockets.route('/submit')
 def inbox(ws):
     """Receives incoming chat messages, inserts them into Redis."""
@@ -82,14 +128,43 @@ def inbox(ws):
             app.logger.info(u'Inserting message: {}'.format(message))
             redis.publish(REDIS_CHAN, message)
 
-@sockets.route('/receive')
-def outbox(ws):
+@sockets.route('/listen')
+def listen(ws):
     """Sends outgoing chat messages, via `ChatBackend`."""
-    chats.register(ws)
+
+    # Save the uuid from the client
+    if 'id' not in request.args:
+        return "No id in request", 400
+    
+    client_id = request.args['id']
+
+    register_worked = chats.add_worker(ws, client_id)
+    if not register_worked:
+        return "Client not registered", 400
 
     while not ws.closed:
         # Context switch while `ChatBackend.start` is running in the background.
         gevent.sleep(0.1)
 
+    chats.remove(client_id)
 
+@app.route('/register')
+def register():
+    """
+    Register a client and get an ID
+    """
 
+    if 'Authorization' not in request.headers:
+        return "Not authorized", 401
+
+    # Check the authorization bearer token
+
+    # Add the client to the chat
+    client_details = request.json()
+    # Generate a uuid for the client and return it
+    client_id = uuid.uuid4()
+    chats.register_worker(client_id, client_details)
+    to_return = {
+        'client_id': client_id
+    }
+    return to_return
