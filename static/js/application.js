e77b59ab050c46e87344c882c5a16742f1eba7e0
Derek Weitzel <djw8605@gmail.com>
2020-10-11 10:09:31 -0500

    Only show the first 8 characters of the id

--- static/js/application.js
+++ static/js/application.js
@@ -8,7 +8,7 @@ $( document ).ready(function() {
       for (const [key, value] of Object.entries(resp)) {
         console.log(`${key}: ${value}`);
         var row = $('<tr/>');
-        row.append($('<td/>').html(key));
+        row.append($('<td/>').html(key.substring(0, 8)));
         row.append($('<td/>').html(value.hostname));
         row.attr('id', key);
         $("#clients").append(row);
